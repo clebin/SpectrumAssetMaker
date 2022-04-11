7562fbcc4efc366126f945ce23dea3501996619f
Chris Owen <chris@page.wales>
2022-04-11 17:20:36 +0100

    Start reading object layers

--- Tilemap.php
+++ Tilemap.php
@@ -9,6 +9,24 @@ class Tilemap {
     // data arrays
     private static $screens = [];
 
+    // object layers
+    private static $screens_objects = [];
+    private static $screens_enemies = [];
+    private static $screen_colours = [];
+
+    // allowed properties on enemies, objects, etc.
+    private static $object_allowed_properties = [
+        'collectable',
+        'deltax', 
+        'deltay', 
+        'speed', 
+        'numhits', 
+        'transient', 
+        'lethal', 
+        'endval', 
+        'movement'
+    ];
+
     /**
      * Read the tilemap JSON file.
      */
@@ -17,35 +35,93 @@ class Tilemap {
         if(!file_exists($filename)) {
             return false;
         }
-
+        
         $json = file_get_contents($filename);
         $data = json_decode($json, true);
 
+        // read with object layers
+        if( isset($data['layers']['layers']) ) {
+            self::ReadFileWithObjects($data);
+        }
+        // read simple
+        else {
+            self::ReadFileSimple($data);
+        }
+    }
+
+    public static function ReadFileSimple($data)
+    {
         // each layer counts as one screen
         foreach($data['layers'] as $layer) {
 
-            // now do paper, ink, etc
-            $screen = [];
-
-            foreach($layer['data'] as $tileNum) {
-
-                $tileNum = intval($tileNum)-1;
-
-                if( Tileset::TileExists($tileNum) === true ) {
-
-                    $screen[] = $tileNum;
-                    
-                } else {
-
-                    $screen[] = 0;
-                    echo 'Warning: '.$tileNum.' not found. '.CR;
-                }
-            }
-            
-            // add to screens
-            self::$screens[] = $screen;
+            self::$screens[] = self::GetLayerTiles($layer);
         }
         return true;
+    }
+
+    public static function ReadFileWithObjects($data)
+    {
+        // loop through groups
+        foreach($data['layers'] as $group) {
+            
+            foreach($group as $layer) {
+
+                // get tilemap and object layers
+                switch( strtolower($layer['name']) ) {
+                    case 'tilemap':
+                        self::$screens[] = self::ReadTilemapLayer($layer);
+                    break;
+
+                    case 'enemies':
+                        self::$screens_objects[] = self::ReadObjectLayer($layer);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static function ReadTilemapLayer($layer)
+    {
+        $screen = [];
+
+        foreach($layer['data'] as $tileNum) {
+
+            $tileNum = intval($tileNum)-1;
+
+            if( Tileset::TileExists($tileNum) === true ) {
+                $screen[] = $tileNum;
+            } else {
+                $screen[] = 0;
+                echo 'Warning: '.$tileNum.' not found. '.CR;
+            }
+        }
+
+        return $screen;
+    }
+
+    public static function ReadObjectLayer($data)
+    {
+        $objects = [];
+        foreach($layer['objects'] as $json_object) {
+
+            $obj = [];
+
+            // type
+            $obj['type'] = $json_object['type'];
+
+            // custom properties
+            foreach($json_object['properties'] as $prop) {
+                $obj[$prop['name']] = $prop['value'];
+            }
+
+            // x and y positions
+            $obj['x'] = intval($json_obj['x']);
+            $obj['y'] = intval($json_obj['y']);
+
+            // add to array
+            $objects[] = $obj;
+        }
+        return $objects;
     }
     
     /**
